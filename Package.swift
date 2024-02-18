85ad3b9bec32bf95b6461718f90aab5a691c6f02
Thomas Papillon <tp@irrealis.me>
2024-02-18 17:18:39 +0100

    build: override dependency

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/amplitude/analytics-connector-ios.git", from: "1.0.1")
+        .package(url: "https://github.com/Thooms/analytics-connector-ios.git", .revision("d71c60e6a4c5cdc54efe2aae3048012b612426e2")) // clone without tests
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
